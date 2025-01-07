1a6351efde907bd620f1856808f37a9611065476
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2025-01-07 06:51:08 +0100

    2024: Optimize and document quest 10

--- 2024/quest10/src/main.rs
+++ 2024/quest10/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::VecDeque, fs};
 
 fn parse_file(filename: &str) -> (Vec<u8>, usize, usize) {
     let input = fs::read_to_string(filename).unwrap();
@@ -19,17 +19,12 @@ fn parse(input: &str) -> (Vec<u8>, usize, usize) {
 fn try_solve(grid: &mut [u8], width: usize, sx: usize, sy: usize) -> Option<(usize, Vec<u8>)> {
     let mut used = vec![vec![false; 8]; 8];
 
+    // 1st pass: find as many pairs as possible
     for y in 2..6 {
         'col: for x in 2..6 {
-            for y1 in 0..8 {
-                if (2..6).contains(&y1) {
-                    continue;
-                }
+            for y1 in [0, 1, 6, 7] {
                 let cy = grid[(sy + y1) * width + sx + x];
-                for x1 in 0..8 {
-                    if (2..6).contains(&x1) {
-                        continue;
-                    }
+                for x1 in [0, 1, 6, 7] {
                     let cx = grid[(sy + y) * width + sx + x1];
                     if cx == cy {
                         used[y][x1] = true;
@@ -42,24 +37,20 @@ fn try_solve(grid: &mut [u8], width: usize, sx: usize, sy: usize) -> Option<(usi
         }
     }
 
+    // 2nd pass: for each blank, fill it if there is a question mark and if
+    // there's exactly one unused symbol
     for y in 2..6 {
         for x in 2..6 {
             if grid[(sy + y) * width + sx + x] != b'.' {
                 continue;
             }
             let mut unused = Vec::new();
-            for y1 in 0..8 {
-                if (2..6).contains(&y1) {
-                    continue;
-                }
+            for y1 in [0, 1, 6, 7] {
                 let cy = grid[(sy + y1) * width + sx + x];
                 if used[y1][x] {
                     continue;
                 }
-                for x1 in 0..8 {
-                    if (2..6).contains(&x1) {
-                        continue;
-                    }
+                for x1 in [0, 1, 6, 7] {
                     if used[y][x1] {
                         continue;
                     }
@@ -75,32 +66,26 @@ fn try_solve(grid: &mut [u8], width: usize, sx: usize, sy: usize) -> Option<(usi
                 let (c, x1, y1) = unused[0];
                 grid[(sy + y) * width + sx + x] = c;
                 grid[(sy + y1) * width + sx + x1] = c;
+                used[y1][x1] = true;
             }
         }
     }
 
-    let mut solvable = true;
+    // check if block is solvable and calculate power
+    let mut power = 0;
     let mut result = Vec::new();
-    'row: for y in 2..6 {
+    for y in 2..6 {
         for x in 2..6 {
             let c = grid[(sy + y) * width + sx + x];
             if c == b'.' {
-                solvable = false;
-                break 'row;
+                // unsolvable
+                return None;
             }
             result.push(c);
+            power += result.len() * (c - b'A' + 1) as usize;
         }
     }
-
-    if solvable {
-        let mut power = 0;
-        for (i, c) in result.iter().enumerate() {
-            power += (i + 1) * (c - b'A' + 1) as usize;
-        }
-        Some((power, result))
-    } else {
-        None
-    }
+    Some((power, result))
 }
 
 fn main() {
@@ -137,22 +122,35 @@ fn main() {
     let w = width3 / 6;
     let h = height3 / 6;
 
-    let mut max_total = 0;
-    loop {
-        let mut total = 0;
-        for r in 0..h {
-            for c in 0..w {
-                if let Some((power, _)) = try_solve(&mut grid3, width3, c * 6, r * 6) {
-                    total += power;
-                }
+    // solve as many blocks as possible
+    let mut queue = VecDeque::new();
+    let mut total = 0;
+    for r in 0..h {
+        for c in 0..w {
+            if let Some((power, _)) = try_solve(&mut grid3, width3, c * 6, r * 6) {
+                total += power;
+            } else {
+                queue.push_back((r, c));
             }
         }
+    }
 
-        if total > max_total {
-            max_total = total;
-        } else {
+    // keep on trying to solve until all remaining blocks are unsolvable
+    loop {
+        let mut solved_more = false;
+        let mut queue2 = VecDeque::new();
+        while let Some((r, c)) = queue.pop_front() {
+            if let Some((power, _)) = try_solve(&mut grid3, width3, c * 6, r * 6) {
+                total += power;
+                solved_more = true;
+            } else {
+                queue2.push_back((r, c));
+            }
+        }
+        if !solved_more {
             break;
         }
+        queue = queue2;
     }
-    println!("{}", max_total);
+    println!("{}", total);
 }
