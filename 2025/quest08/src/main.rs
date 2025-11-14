5ce82d81ddc8bb1c41b10b8a495e743d91ab1aaa
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2025-11-14 15:45:20 +0100

    2025: Optimize quest 8

--- 2025/quest08/src/main.rs
+++ 2025/quest08/src/main.rs
@@ -23,6 +23,22 @@ fn count_intersections(new_thread: (usize, usize), threads: &[(usize, usize)]) -
     result
 }
 
+fn add_range(grid: &mut [i64], x1: usize, y1: usize, x2: usize, y2: usize, size: usize) {
+    if x1 >= size || y1 >= size {
+        return;
+    }
+    grid[y1 * size + x1] += 1;
+    if x2 < size {
+        grid[y1 * size + x2] -= 1;
+    }
+    if y2 < size {
+        grid[y2 * size + x1] -= 1;
+    }
+    if x2 < size && y2 < size {
+        grid[y2 * size + x2] += 1;
+    }
+}
+
 fn main() {
     // part 1
     let input = parse("everybody_codes_e2025_q08_p1.txt");
@@ -50,21 +66,39 @@ fn main() {
     println!("{total}");
 
     // part 3
+    const SIZE: usize = 256;
     let input = parse("everybody_codes_e2025_q08_p3.txt");
-    let mut threads: Vec<(usize, usize)> = Vec::new();
-    let mut total = 0;
+    let mut grid = vec![0i64; SIZE * SIZE];
     for w in input.windows(2) {
-        let new_thread = if w[0] < w[1] {
-            (w[0], w[1])
+        let t = if w[0] < w[1] {
+            (w[0] - 1, w[1] - 1)
         } else {
-            (w[1], w[0])
+            (w[1] - 1, w[0] - 1)
         };
-        threads.push(new_thread);
+        let y1 = t.0 + 1;
+        let x1 = t.1 + 1;
+        let y2 = t.1;
+        let x2 = t.0;
+        add_range(&mut grid, x1, y1, SIZE + 1, y2, SIZE);
+        add_range(&mut grid, y1, 0, y2, x2, SIZE);
+        add_range(&mut grid, t.1, t.0, t.1 + 1, t.0 + 1, SIZE);
     }
-    for a in 1..=256 {
-        for b in a + 1..=256 {
-            total = total.max(count_intersections((a, b), &threads));
+
+    let mut max = 0;
+    for y in 0..SIZE {
+        let mut sum = 0;
+        for x in 0..SIZE {
+            sum += grid[y * SIZE + x];
+            grid[y * SIZE + x] = sum;
         }
     }
-    println!("{total}");
+    for x in 0..SIZE {
+        let mut sum = 0;
+        for y in 0..SIZE {
+            sum += grid[y * SIZE + x];
+            max = max.max(sum);
+        }
+    }
+
+    println!("{max}");
 }
